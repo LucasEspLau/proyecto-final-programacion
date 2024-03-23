eca7683f2f1becec2b98123fb9e9ff7829186c27
Aitor Palomino <practicante4@domain.com.pe>
2024-03-22 19:49:29 -0500

    Avance de código

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace trabajo_final_grupo_verde.Data;
 
+
+    //Controlador
 public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,6 +3,6 @@ namespace trabajo_final_grupo_verde.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
-
+    
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
