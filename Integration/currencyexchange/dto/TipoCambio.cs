a28c8a8fbacf3f0d8d5fb26054292e48e94a04ee
= <=>
2024-05-23 12:55:35 -0500

    Implementacion de la API EXCHANGE

---
+++ Integration/currencyexchange/dto/TipoCambio.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace trabajo_final_grupo_verde.Integration.currencyexchange.dto
+{
+
+    public class TipoCambio
+    {
+        public int Cantidad { get; set; }
+        public string? From { get; set; }
+        public string? To { get; set; }
+    }
+
+
+}
